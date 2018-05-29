334477f2e88c48955d93ba206363e771f5c34ee3
jano42 <decoretjm@gmail.com>
2018-05-29 07:47:49 +0200

    Ensure entrypoints are executables.\nAdd pushcontainers scripts.

---
+++ build-linux/pushContainer.ps1
@@ -0,0 +1,2 @@
+docker tag build_for_ubuntu yadoms/build_for_ubuntu
+docker push yadoms/build_for_ubuntu
---
+++ build-raspberrypi/pushContainer.ps1
@@ -0,0 +1,2 @@
+docker tag build_for_raspberrypi yadoms/build_for_raspberrypi
+docker push yadoms/build_for_raspberrypi
