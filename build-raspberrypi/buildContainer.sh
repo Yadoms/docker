f3875b1b442408585ba75913c40a50e4abc5d0fc
jano42 <decoretjm@gmail.com>
2019-06-13 17:27:38 +0200

    Fix build-rapsberry docker file (due to git fetch pb, now only clone yadoms in entrypoint). Add userTests dockerfile

--- build-raspberrypi/buildContainer.sh
+++ build-raspberrypi/buildContainer.sh
@@ -1,5 +1,5 @@
-!#/bin/bash
+#!/bin/bash
 
-sudo docker build --build-arg http_proxy=http://xrxproxy.acs-inc.fr:8080 --build-arg https_proxy=http://xrxproxy.acs-inc.fr:8080 -t build_for_raspberrypi .
+docker build --build-arg http_proxy=http://xrxproxy.acs-inc.fr:8080 --build-arg https_proxy=http://xrxproxy.acs-inc.fr:8080 -t build_for_raspberrypi .
 
 pause
