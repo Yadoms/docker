594f5bd6faac765ec643a1ae826b44bf1d3b01a9
jano42 <decoretjm@gmail.com>
2019-12-12 16:19:08 +0100

    Move update package to yadoms sources

--- build-linux/entrypoint.sh
+++ build-linux/entrypoint.sh
@@ -2,8 +2,14 @@
 set -e
 
 # Yadoms sources
-echo "Update Yadoms Git repository for $YADOMS_BUILD_BRANCH branch"
-git clone -b $YADOMS_BUILD_BRANCH https://github.com/Yadoms/yadoms.git
+if [ -z "$YADOMS_REPO" ]
+then
+	echo "Update Yadoms Git repository for $YADOMS_BUILD_BRANCH branch"
+	git clone -b $YADOMS_BUILD_BRANCH https://github.com/Yadoms/yadoms.git
+else
+	echo "Update Yadoms Git repository for $YADOMS_REPO:$YADOMS_BUILD_BRANCH branch"
+	git clone -b $YADOMS_BUILD_BRANCH https://github.com/$YADOMS_REPO
+fi
 
 cd yadoms
 
@@ -27,25 +33,8 @@ if [ $MAKE_PACKAGE == "true" ]; then
 	make package
 	cd -
 	
-	echo "Build Yadoms update package"
-	mkdir updatepackage
-	yadomsVersion=$(grep -oP '###[[:space:]]\K.*' sources/server/changelog.md -m 1)
-	# Copy script
-	cp update/scripts/update.sh updatepackage/update.sh
-	# Generate package.json
-	cp update/package.json.in updatepackage/package.json
-	sed -i -- 's/__version__/'$yadomsVersion'/g' updatepackage/package.json
-	sed -i -- 's/__gitdate__/'`git log -1 --format=%cI `'/g' updatepackage/package.json
-	cp sources/server/changelog.md updatepackage/changelog.md
-	mv builds/package packagetomove
-	mv builds updatepackage/package
-	rm -f updatepackage/package/yadoms.ini
-	cd updatepackage
-	zip -r ../package.zip ./ -x \*.gitignore
-	cd -
-	mkdir builds
-	mv packagetomove builds/package
-	mv package.zip builds/package
+	cd update
+	sh make_package.sh Linux
 	
 	if [ ! -z "$UPLOAD_FTP_CREDENTIALS" ]; then
 		echo "Upload packages"
