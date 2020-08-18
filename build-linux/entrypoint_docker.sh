15c8da9fa6016faca66699c179bebd773c88421f
jano42 <decoretjm@gmail.com>
2020-08-18 07:55:42 +0200

    Add entrypoint for docker in github action

---
+++ build-linux/entrypoint_docker.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+cd yadoms
+
+echo "Copy build config file"
+cp $YADOMS_DEPS_PATH/CMakeListsUserConfig.txt sources/
+
+echo "Display config content"
+cat sources/CMakeListsUserConfig.txt
+
+echo "Create makefile"
+sh cmake_linux.sh r
+
+echo "Build Yadoms"
+cd projects
+make all_unity
+cd -
+
+if [ $MAKE_PACKAGE == "true" ]; then
+	echo "Build Yadoms package"
+	cd projects
+	make package
+	cd -
+	
+	cd update
+	sh make_package.sh Linux
+fi
